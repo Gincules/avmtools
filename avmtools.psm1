16ede646e5d5ffc44278ec3d939f3514d4ca8461
Gincules <lukas.stoerzenhofecker@gmail.com>
2022-10-17 15:47:25 +0200

    added support for PSVersion5 and PSVersion7

--- avmtools.psm1
+++ avmtools.psm1
@@ -1,7 +1,7 @@
 $psMajor = $PSVersionTable.PSVersion.Major
 $psMinor = $PSVersionTable.PSVersion.Minor
 
-if (( $psMajor -ge 7) -And ($psMinor -ge 2)) {
+if (( $psMajor -ge 7) -And ($psMinor -ge 0)) {
     (Get-ChildItem -Path "$PSScriptRoot\Functions\PSVersion7" -Recurse -Filter *.ps1) | ForEach-Object {
         . $_.FullName
     }
