237cb124f6f6c654c23cc8d01e3da58e0c073135
Gincules <lukas.stoerzenhofecker@gmail.com>
2022-10-22 09:56:09 +0200

    updated some functions

--- Functions/PSVersion7/Invoke-AvmAction.ps1
+++ Functions/PSVersion7/Invoke-AvmAction.ps1
@@ -34,33 +34,42 @@ function Invoke-AvmAction {
 
 	Param
 	(
+		[Alias("I","i")]
 		[Parameter()]
 		[System.Management.Automation.SwitchParameter]$Insecure,
 
+		[Alias("U","u")]
 		[Parameter(Mandatory)]
 		[ValidateNotNullOrEmpty()]
 		[System.String]$Url,
 
+		[Alias("P","p")]
 		[Parameter(Mandatory)]
 		[ValidateRange(0,65535)]
 		[System.UInt16]$Port,
 
+		[Alias("C","c")]
 		[Parameter(Mandatory)]
 		[ValidateNotNullOrEmpty()]
 		[System.Management.Automation.PSCredential]$Credential,
 
+		# since U|u for url and P|p for path, S|s for segemet or subdirectory is already taken - D|d like directory
+		[Alias("D","d")]
 		[Parameter(Mandatory)]
 		[ValidateNotNullOrEmpty()]
 		[System.String]$UrlPath,
 
+		[Alias("S","s")]
 		[Parameter(Mandatory)]
 		[ValidateNotNullOrEmpty()]
 		[System.String]$SoapAction,
 
+		[Alias("B","b")]
 		[Parameter(Mandatory)]
 		[ValidateNotNullOrEmpty()]
 		[System.Xml.XmlNode]$Body,
 
+		[Alias("X","x")]
 		[Parameter(Mandatory)]
 		[ValidateNotNullOrEmpty()]
 		[System.String]$XmlResponse
@@ -84,31 +93,39 @@ function Invoke-AvmAction {
 
 	Process {
 		# PowerShell 7.2
-		[System.Object]$webResponse = Try { if ($Insecure) { [System.Xml.XmlDocument]$avmResponse = Invoke-RestMethod @splatParameters -AllowUnencryptedAuthentication -ErrorVariable webError } else { [System.Xml.XmlDocument]$avmResponse = Invoke-RestMethod @splatParameters -ErrorVariable webError } } Catch { $_.Exception.Response }
+		# call TR-064 api and catch error
+		[System.Object]$webResponse = Try { if ($Insecure) { [System.Xml.XmlDocument]$avmResponse = Invoke-RestMethod @splatParameters -AllowUnencryptedAuthentication -ErrorVariable webError } else { [System.Xml.XmlDocument]$avmResponse = Invoke-RestMethod @splatParameters -ErrorVariable webError }} Catch { $_.Exception.Response }
 
+		# assign status of web request
 		if ($statusCode -eq 200) {
 			[System.String]$statusDescription = "OK"
 		} else {
 			$statusCode = $webResponse.StatusCode.value__
+			#[System.Text.StringBuilder]$statusDescription = $webResponse.StatusCode
 			[System.String]$statusDescription = $webResponse.StatusCode
 			
+			# remove new lines from error message
 			$statusDescription += " (" + ($webError.Message -Replace("`n`n| ") -Replace "`n", " ").Trim() + ")"
 		}
 
+		# use XML for consistency
 		[System.Xml.XmlDocument]$statusXml = "<?xml version=`"1.0`"?><Envelope><Body><$XmlResponse><statusCode>$statusCode</statusCode><statusDescription>$statusDescription</statusDescription></$XmlResponse></Body></Envelope>"
 
-		[System.Array]$finalResult = @()
+		[System.Collections.ArrayList]$finalResult = @()
 
-		$avmResponse.Envelope.Body.$XmlResponse | Select-Object | ForEach-Object {
-			$finalResult += $_
+		# append response of AVM device
+		ForEach ($object in $avmResponse.Envelope.Body.$XmlResponse) {
+			[System.Void]$finalResult.Add($object)
 		}
 
-		$statusXml.Envelope.Body.$XmlResponse | Select-Object | ForEach-Object {
-			$finalResult += $_
+		# append status code and message
+		ForEach ($object in $statusXml.Envelope.Body.$XmlResponse) {
+			[System.Void]$finalResult.Add($object)
 		}
 	}
 
 	End {
+		# return the result
 		return (! ([System.String]::IsNullOrEmpty($finalResult))) ? $finalResult : $false
 	}
 }
--- avmtools.psm1
+++ avmtools.psm1
@@ -1,20 +1,17 @@
-$psMajor = $PSVersionTable.PSVersion.Major
-$psMinor = $PSVersionTable.PSVersion.Minor
-
-(Get-ChildItem -Path "$PSScriptRoot\Classes\Generic" -Recurse -Filter *.ps1) | ForEach-Object {
-    . $_.FullName
+ForEach ($object in (Get-ChildItem -Path "$PSScriptRoot\Classes\Generic" -Recurse -Filter *.ps1)) {
+    . $object.FullName
 }
 
-if (( $psMajor -ge 7) -And ($psMinor -ge 0)) {
-    (Get-ChildItem -Path "$PSScriptRoot\Functions\PSVersion7" -Recurse -Filter *.ps1) | ForEach-Object {
-        . $_.FullName
+if ($PSVersionTable.PSVersion.Major -gt 5) {
+    ForEach ($object in (Get-ChildItem -Path "$PSScriptRoot\Functions\PSVersion7" -Recurse -Filter *.ps1)) {
+        . $object.FullName
     }
 } else {
-    (Get-ChildItem -Path "$PSScriptRoot\Functions\PSVersion5" -Recurse -Filter *.ps1) | ForEach-Object {
-        . $_.FullName
+    ForEach ($object in (Get-ChildItem -Path "$PSScriptRoot\Functions\PSVersion5" -Recurse -Filter *.ps1)) {
+        . $object.FullName
     }
 }
 
-(Get-ChildItem -Path "$PSScriptRoot\Functions\Generic" -Recurse -Filter *.ps1) | ForEach-Object {
-    . $_.FullName
+ForEach ($object in (Get-ChildItem -Path "$PSScriptRoot\Functions\Generic" -Recurse -Filter *.ps1)) {
+    . $object.FullName
 }
