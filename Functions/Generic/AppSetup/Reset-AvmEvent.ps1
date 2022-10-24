34a62ee162c518435056b94f19a32c165f6022fd
Gincules <lukas.stoerzenhofecker@gmail.com>
2022-10-24 16:59:42 +0200

    updated functions and their documentation

--- Functions/Generic/AppSetup/Reset-AvmEvent.ps1
+++ Functions/Generic/AppSetup/Reset-AvmEvent.ps1
@@ -1,19 +1,9 @@
 function Reset-AvmEvent {
     <#
         .SYNOPSIS
-            Reset FRITZ!Box app event by id
+            Wiki: https://github.com/Gincules/avmtools/wiki/Reset-AvmEvent
         .DESCRIPTION
-            Resets FRITZ!Box app event by id
-        .PARAMETER Insecure
-            Use unencrypted authentication over http instead of https
-        .PARAMETER Url
-            Url of FRITZ!Box
-        .PARAMETER Port
-            Port of FRITZ!Box
-        .PARAMETER Credential
-            PSCredential variable
-        .PARAMETER EventId
-            Identifier of the event instance
+            Wiki: https://github.com/Gincules/avmtools/wiki/Reset-AvmEvent
         .NOTES
             Author: Gincules
             Website: https://github.com/Gincules/avmtools
@@ -22,37 +12,41 @@ function Reset-AvmEvent {
             https://github.com/Gincules/avmtools
             https://github.com/Gincules/avmtools/blob/main/LICENSE
         .EXAMPLE
-            PS C:\> [PSCredential]$Credential = Get-Credential
-            PS C:\> Reset-AvmEvent -Url "https://fritz.box" -Port 49443 -Credential $Credential
-        .EXAMPLE
-            PS C:\> [PSCredential]$Credential = Get-Credential
-            PS C:\> Reset-AvmEvent -Insecure -Url "http://fritz.box" -Port 49000 -Credential $Credential
-        .EXAMPLE
-            PS C:\> [PSCredential]$Credential = Get-Credential
-            PS C:\> Reset-AvmEvent -Url "https://192.168.178.1" -Port 49443 -Credential $Credential
-        .EXAMPLE
-            PS C:\> [PSCredential]$Credential = Get-Credential
-            PS C:\> Reset-AvmEvent -Insecure -Url "http://192.168.178.1" -Port 49000 -Credential $Credential
+            Wiki: https://github.com/Gincules/avmtools/wiki/Reset-AvmEvent
     #>
 
     Param
     (
-        [switch]$Insecure = $false,
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$Url,
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][int32]$Port,
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][PSCredential]$Credential,
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][int32]$EventId
+        [Parameter()]
+        [System.Management.Automation.SwitchParameter]$Insecure = $false,
+
+        [Parameter()]
+        [System.Management.Automation.SwitchParameter]$RemoteAccess= $false,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]$Url,
+
+        [Parameter(Mandatory)]
+        [ValidateRange(0,65535)]
+        [System.UInt16]$Port,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]$Credential,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateRange(0,4294967295)]
+        [System.Uint32]$NewEventId
     )
 
     Begin {
         $avmWebrequestBody = [AvmBody]::new()
 
         $avmWebrequestBody.SoapAction = "urn:dslforum-org:service:X_AVM-DE_AppSetup:1"
+        $avmWebrequestBody.UrlPath = "/upnp/control/x_appsetup"
         $avmWebrequestBody.Action = "ResetEvent"
-        $avmWebrequestBody.InnerBody = "<s:NewEventId>{0}</s:NewEventId>" -f $EventId
-
-        [xml]$avmBodyParameter = $avmWebrequestBody.GenerateBody()
-        [string]$soapAction = $avmWebrequestBody.GenerateSoapAction()
+        $avmWebrequestBody.InnerBody = "<s:NewEventId>{0}</s:NewEventId>" -f $NewEventId
     }
 
     Process {
@@ -61,10 +55,10 @@ function Reset-AvmEvent {
             Url = $Url
             Port = $Port
             Credential = $Credential
-            Body = $avmBodyParameter
-            SoapAction = $soapAction
-            UrlPath = "/upnp/control/x_appsetup"
-            XmlResponse = "ResetEventResponse"
+            Body = $avmWebrequestBody.GenerateBody()
+            SoapAction = $avmWebrequestBody.GenerateSoapAction()
+            UrlPath = $avmWebrequestBody.UrlPath
+            XmlResponse = $avmWebrequestBody.GenerateXmlResponse()
         }
 
         Invoke-AvmAction @splatParameters
--- Functions/Generic/AppSetup/Set-AvmAppMessageFilter.ps1
+++ Functions/Generic/AppSetup/Set-AvmAppMessageFilter.ps1
@@ -1,27 +1,9 @@
 function Set-AvmAppMessageFilter {
     <#
         .SYNOPSIS
-            Set FRITZ!Box app message filter
+            Wiki: https://github.com/Gincules/avmtools/wiki/Set-AvmAppMessageFilter
         .DESCRIPTION
-            Set FRITZ!Box app message filter
-        .PARAMETER Insecure
-            Use unencrypted authentication over http instead of https
-        .PARAMETER RemoteAccess
-            Access FRITZ!Box from the internet
-        .PARAMETER Url
-            Url of FRITZ!Box
-        .PARAMETER Port
-            Port of FRITZ!Box
-        .PARAMETER Credential
-            PSCredential variable
-        .PARAMETER AppId
-            Identifier of the app instance the message filter belongs to
-        .PARAMETER AppType
-            Type of the message filter, eg. “aha_ident” 
-        .PARAMETER AppFilter
-            A stringlist of filter criteria
-            (e.g. “08761 0000444,34:45:12:43:55” for type aha_ident)
-            Allowed characters: a-Z,0-9, Space and ,+:-_
+            Wiki: https://github.com/Gincules/avmtools/wiki/Set-AvmAppMessageFilter
         .NOTES
             Author: Gincules
             Website: https://github.com/Gincules/avmtools
@@ -30,47 +12,53 @@ function Set-AvmAppMessageFilter {
             https://github.com/Gincules/avmtools
             https://github.com/Gincules/avmtools/blob/main/LICENSE
         .EXAMPLE
-            PS C:\> [PSCredential]$Credential = Get-Credential
-            PS C:\> Set-AvmAppMessageFilter -RemoteAccess -Url "https://myfritzaddress12.myfritz.net" -Port 443 -Credential $Credential
-        .EXAMPLE
-            PS C:\> [PSCredential]$Credential = Get-Credential
-            PS C:\> Set-AvmAppMessageFilter -Url "https://fritz.box" -Port 49443 -Credential $Credential
-        .EXAMPLE
-            PS C:\> [PSCredential]$Credential = Get-Credential
-            PS C:\> Set-AvmAppMessageFilter -Insecure -Url "http://fritz.box" -Port 49000 -Credential $Credential
-        .EXAMPLE
-            PS C:\> [PSCredential]$Credential = Get-Credential
-            PS C:\> Set-AvmAppMessageFilter -Url "https://192.168.178.1" -Port 49443 -Credential $Credential
-        .EXAMPLE
-            PS C:\> [PSCredential]$Credential = Get-Credential
-            PS C:\> Set-AvmAppMessageFilter -Insecure -Url "http://192.168.178.1" -Port 49000 -Credential $Credential
+            Wiki: https://github.com/Gincules/avmtools/wiki/Set-AvmAppMessageFilter
     #>
 
     Param
     (
-        [switch]$Insecure = $false,
-        [switch]$RemoteAccess = $false,
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$Url,
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][int32]$Port,
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][PSCredential]$Credential,
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][int32]$AppId,
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$AppType,
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$AppFilter
+        [Parameter()]
+        [System.Management.Automation.SwitchParameter]$Insecure = $false,
+
+        [Parameter()]
+        [System.Management.Automation.SwitchParameter]$RemoteAccess= $false,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]$Url,
+
+        [Parameter(Mandatory)]
+        [ValidateRange(0,65535)]
+        [System.UInt16]$Port,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]$Credential,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]$NewAppId,
+
+        [Parameter(Mandatory)]
+        [ValidateSet("aha_ident","tel_local_number")]
+        [System.String]$NewType,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]$NewFilter
     )
 
     Begin {
         $avmWebrequestBody = [AvmBody]::new()
 
         $avmWebrequestBody.SoapAction = "urn:dslforum-org:service:X_AVM-DE_AppSetup:1"
+        $avmWebrequestBody.UrlPath = "$(if ($RemoteAccess) { "/tr064" })/upnp/control/x_appsetup"
         $avmWebrequestBody.Action = "SetAppMessageFilter"
         $avmWebrequestBody.InnerBody = @"
 <s:AppId>{0}</s:AppId>
 <s:Type>{1}</s:Type>
 <s:NewEventId>{2}</s:NewEventId>
-"@ -f $AppId, $AppType, $AppFilter
-
-        [xml]$avmBodyParameter = $avmWebrequestBody.GenerateBody()
-        [string]$soapAction = $avmWebrequestBody.GenerateSoapAction()
+"@ -f $NewAppId, $NewType, $NewFilter
     }
 
     Process {
@@ -79,10 +67,10 @@ function Set-AvmAppMessageFilter {
             Url = $Url
             Port = $Port
             Credential = $Credential
-            Body = $avmBodyParameter
-            SoapAction = $soapAction
-            UrlPath = "$(if ($RemoteAccess) { "/tr064" })/upnp/control/x_appsetup"
-            XmlResponse = "SetAppMessageFilterResponse"
+            Body = $avmWebrequestBody.GenerateBody()
+            SoapAction = $avmWebrequestBody.GenerateSoapAction()
+            UrlPath = $avmWebrequestBody.UrlPath
+            XmlResponse = $avmWebrequestBody.GenerateXmlResponse()
         }
 
         Invoke-AvmAction @splatParameters
