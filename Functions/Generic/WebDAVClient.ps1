9fb3d71ab04d741c44c7091b0f74f66c6c3db7b9
Gincules <gincules@gmail.com>
2023-01-28 12:51:54 +0100

    Updated and fixed a function

--- Functions/Generic/WebDAVClient.ps1
+++ Functions/Generic/WebDAVClient.ps1
@@ -70,35 +70,35 @@ function Set-AvmWebDavClientConfig {
         .EXAMPLE
             Wiki: https://github.com/Gincules/avmtools/wiki/Set-AvmWebDavClientConfig
     #>
-    
+
     Param
     (
         [Alias("i")]
         [Parameter()]
         [System.Management.Automation.SwitchParameter]$Insecure = $false,
-    
+
         [Alias("r")]
         [Parameter()]
         [System.Management.Automation.SwitchParameter]$RemoteAccess = $false,
-    
+
         [Alias("u")]
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [System.String]$Url,
-    
+
         [Alias("p")]
         [Parameter(Mandatory)]
         [ValidateRange(0,65535)]
         [System.UInt16]$Port,
-    
+
         [Alias("c")]
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [System.Management.Automation.PSCredential]$Credential,
-    
+
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [System.String]$NewEnable,
+        [System.Byte][System.Boolean]$NewEnable,
 
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
@@ -106,28 +106,36 @@ function Set-AvmWebDavClientConfig {
 
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [System.Management.Automation.PSCredential]$NewCredential,
+        [System.String]$NewUsername,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [System.Security.SecureString]$NewPassword,
 
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [System.String]$NewMountpointName
     )
-    
+
     Begin {
-        $AvmWebrequestBody = [AvmBody]::new()
-    
+        $SecurePointer = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($NewPassword)
+        $PlainNewPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto($SecurePointer)
+        [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($SecurePointer)
+
+        $avmWebrequestBody = [AvmBody]::new()
+
         $AvmWebrequestBody.SoapAction = "urn:dslforum-org:service:X_AVM-DE_WebDAVClient:1"
         $AvmWebrequestBody.UrlPath = "$(if ($RemoteAccess) { "/tr064" })/upnp/control/x_webdav"
-        $AvmWebrequestBody.Action = "X_AVM_DE_SetAutoDisconnectTimeSpan"
+        $AvmWebrequestBody.Action = "SetConfig"
         $AvmWebrequestBody.InnerBody = @"
 <s:NewEnable>{0}</s:NewEnable>
 <s:NewHostURL>{1}</s:NewHostURL>
 <s:NewUsername>{2}</s:NewUsername>
 <s:NewPassword>{3}</s:NewPassword>
 <s:NewMountpointName>{4}</s:NewMountpointName>
-"@ -f $NewEnable, $NewHostURL, $NewCredential.GetNetworkCredential().UserName, $NewCredential.GetNetworkCredential().Password, $NewMountpointName
+"@ -f $NewEnable, $NewHostURL, $NewUsername, $PlainNewPassword, $NewMountpointName
     }
-    
+
     Process {
         $SplatParameters = @{
             Insecure = $Insecure
